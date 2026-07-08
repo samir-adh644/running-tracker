b8962de11bfd69b0a12efbcd4bb5d8b4d431d34c
Samir Adhikari <081bct071@ioepc.edu.np>
2026-07-08 21:51:09 +0545

    basic folder structure made

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const TabLayout = () => {
+  return (
+    <View>
+      <Text>TabLayout</Text>
+    </View>
+  );
+};
+
+export default TabLayout;
---
+++ src/app/(tabs)/stats.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const stats = () => {
+  return (
+    <View>
+      <Text>stats</Text>
+    </View>
+  );
+};
+
+export default stats;
